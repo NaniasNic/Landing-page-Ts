4a13362344591bb2948412aecd957d73825310ff
NaniasNic <ananiasdias0@gmail.com>
2022-09-25 23:13:08 -0300

    reconfig da aplicação

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="">
+     <h1>teste</h1>
+    </div>
+  )
+}
+
+export default App
--- Client/tailwind.config.cjs
+++ Client/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
-    './index.html'
+    './index.html',
   ],
   theme: {
     extend: {},
